ac584b9544bd235bb4369dd06656e2dcf4aa3d32
Gamal Abdul Nasser <gamalanpro@gmail.com>
2018-10-03 17:22:30 +0700

    update structure, add description in readme

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # Boilerplate
+
+- Clone repo
+- Run composer update
+- Set your vhost to point to project_root/public
+- Don't forget to create .env file(copy from .env.example)
+- Open the browser
---
+++ app/router/MainRouter.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: gamalan
+ * Date: 07/10/16
+ * Time: 10:24
+ */
+
+namespace Application\Router;
+
+use Phalcon\Mvc\Router\Group;
+
+class MainRouter extends Group
+{
+    public function initialize()
+    {
+        $this->setPaths([
+            'namespaces' => 'Application\\Controllers',
+            'controller'=>'index'
+        ]);
+
+        $this->add(
+            '/',
+            [
+                'action' => 'helloworld'
+            ]
+        );
+    }
+}
